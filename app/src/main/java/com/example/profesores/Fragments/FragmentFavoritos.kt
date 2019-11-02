55c2076ba9c09125b190659119e2e26a6727af7d
RaulMendezA <rulax2313@gmail.com>
2019-11-01 21:17:27 -0600

    .

--- app/src/main/java/com/example/profesores/Fragments/FragmentFavoritos.kt
+++ app/src/main/java/com/example/profesores/Fragments/FragmentFavoritos.kt
@@ -5,26 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.profesores.Fragments.profesores.ProfesoresContract
 import com.example.profesores.R
+import com.example.profesores.adapters.AdapterCurso
 import com.example.profesores.adapters.AdapterFavoritos
 import org.jetbrains.anko.find
 
-class FragmentFavoritos : Fragment() {
-    private lateinit var mAdapter: AdapterFavoritos //
-    private lateinit var mRecycler: RecyclerView
-
+class FragmentFavoritos : Fragment(), ProfesoresContract.View {
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_favoritos, container, false)
-        /* TODO fetch data from parse and replace it inside AdapterRecipe*/ //de tarea previa
-        mAdapter = AdapterFavoritos(arrayListOf()) //
-        mRecycler = view.find(R.id.fragment_favoritos_rv_favoritos)
-        mRecycler.adapter = mAdapter
-        mRecycler.layoutManager = LinearLayoutManager(view.context, LinearLayoutManager.HORIZONTAL, false)
+        val title = view.findViewById<TextView>(R.id.fragment_favoritos_tv_title) //profesores
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.activity_name_courses_rv)
+        val names = arrayListOf<HashMap<String, String>>()
+        names.add(HashMap())
+        names[0].put("name", "Erick")
+        names[0].put("lastName", "De Santiago")
+        names.add(HashMap())
+        names[1].put("name", "Raul")
+        names[1].put("lastName", "Alvarez")
+        names.add(HashMap())
+        names[2].put("name", "Luis")
+        names[2].put("lastName", "Beltran")
+        names.add(HashMap())
+        names[3].put("name", "Javier")
+        names[3].put("lastName", "Torres")
+        names.add(HashMap())
+        names[4].put("name", "Isaac")
+        names[4].put("lastName", "Cabrera")
+        recyclerView.adapter = AdapterCurso(names)
+        recyclerView.layoutManager = LinearLayoutManager(view.context)
         return view
     }
 }
--- app/src/main/java/com/example/profesores/Fragments/FragmentUsuario.kt
+++ app/src/main/java/com/example/profesores/Fragments/FragmentUsuario.kt
@@ -5,27 +5,46 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.profesores.Fragments.profesores.ProfesoresContract
 import com.example.profesores.R
+import com.example.profesores.adapters.AdapterCurso
 import org.jetbrains.anko.find
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.support.v4.find
 import org.jetbrains.anko.support.v4.startActivity
 
-class FragmentUsuario : Fragment() {
+class FragmentUsuario : Fragment(), ProfesoresContract.View {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_usuario, container, false)
+        val title = view.findViewById<TextView>(R.id.fragment_usuario_tv_title) //profesores
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.activity_name_courses_rv)
+        val names = arrayListOf<HashMap<String, String>>()
+        names.add(HashMap())
+        names[0].put("name", "Erick")
+        names[0].put("lastName", "De Santiago")
+        names.add(HashMap())
+        names[1].put("name", "Raul")
+        names[1].put("lastName", "Alvarez")
+        names.add(HashMap())
+        names[2].put("name", "Luis")
+        names[2].put("lastName", "Beltran")
+        names.add(HashMap())
+        names[3].put("name", "Javier")
+        names[3].put("lastName", "Torres")
+        names.add(HashMap())
+        names[4].put("name", "Isaac")
+        names[4].put("lastName", "Cabrera")
+        recyclerView.adapter = AdapterCurso(names)
+        recyclerView.layoutManager = LinearLayoutManager(view.context)
         return view
     }
 }
-/*
-    override fun onCreate(savedInstanceState: Bundle?) {
-        mCerrar = find(R.id.textView4)
-        mCerrar.setOnClickListener(this)
-    }
-
- */
